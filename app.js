657d8304189d70500e8bf63727f8f91eca78f344
Aziz ullah <azizullahyaqooby@gmail.com>
2020-04-21 13:30:49 +0800

    third

--- app.js
+++ app.js
@@ -8,16 +8,38 @@ var budgetController = (function(){
 
 // THE UI CONTROLLER
 var UIController = (function() {
-    //some code
+    
+    var DOMStrings = {
+        inputType: '.add_type',
+        inputDescription: '.add_description',
+        inputValue: '.add_value',
+        inputBtn: '.add_btn'
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value,
+            }
+        },
+        getDOMstrings: function(){
+            return DOMStrings
+        }
+    }
 })();
 
 
 //THE GLOBEL CONTROLLER
 var controller = ( function(budgetCtrl, UICtrl){
 
+    var DOM = UIController.getDOMstrings()
+
     var ctrlAddItem = function(){
         // 1. Get the field input data.
-
+        var input = UICtrl.getInput()
+        console.log(input)
         // 2. Add the item to the budget controller.
 
         // 3. Add the item to the UI.
@@ -25,11 +47,9 @@ var controller = ( function(budgetCtrl, UICtrl){
         // 4. Claculate the budget.
 
         // 5. Display the budget.
-        console.log('it works')
-
     }
 
-    document.querySelector('.add_btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function(event){
 
