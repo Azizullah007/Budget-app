f1f472fdc630111d8fbbdb2006b1a39e3e1d7743
Aziz ullah <azizullahyaqooby@gmail.com>
2020-04-24 04:11:29 +0800

    eighth

--- app.js
+++ app.js
@@ -100,6 +100,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
         },
 
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue)
+
+            fieldsArr = Array.prototype.slice.call(fields)
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = ""
+            })
+
+            fieldsArr[0].focus()
+        },
+
         getDOMstrings: function(){
             return DOMstrings
         }
@@ -132,9 +146,13 @@ var controller = ( function(budgetCtrl, UICtrl){
 
         // 3. Add the item to the UI.
         UICtrl.addListItem(newItem, input.type)
-        // 4. Claculate the budget.
 
-        // 5. Display the budget.
+        // 4. Clear the text field.
+        UICtrl.clearFields()
+
+        // 5. Claculate the budget.
+
+        // 6. Display the budget.
     }
 
     return{
