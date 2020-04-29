a948c5b748d82a8249a7f565b01a541fd4346348
Aziz ullah <azizullahyaqooby@gmail.com>
2020-04-30 00:17:53 +0800

    last one

--- app.js
+++ app.js
@@ -182,6 +182,12 @@ var UIController = (function() {
 
     }
 
+    var nodeListForEach = function(list, callback){
+        for (var i=0; i<list.length; i++){
+            callback(list[i], i)
+        }
+    }
+
 
     return {
         getInput: function(){
@@ -253,12 +259,6 @@ var UIController = (function() {
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel)
 
 
-            var nodeListForEach = function(list, callback){
-                for (var i=0; i<list.length; i++){
-                    callback(list[i], i)
-                }
-            }
-
             nodeListForEach(fields, function(current, index){
 
                 if (percentages[index] > 0){
@@ -277,6 +277,22 @@ var UIController = (function() {
             month = now.getMonth()
             year = now.getFullYear()
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year
+        },
+
+        changedType: function (){
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ','+
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            )
+
+            nodeListForEach(fields, function(cur){
+                cur.classList.toggle('red-focus')
+            })
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red')
+
         },
 
         getDOMstrings: function(){
@@ -300,6 +316,8 @@ var controller = ( function(budgetCtrl, UICtrl){
         })
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
     }
 
     var updateBudget = function(){
